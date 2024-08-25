5f7537e76f2c358de0183fa0f39ce38bb1ef152a
anwaar001 <anwaar6260@gmail.com>
2024-08-26 03:19:05 +0500

    added wallet copy

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { HiMenu } from "react-icons/hi";
 import { createPortal } from "react-dom";
@@ -6,10 +6,15 @@ import { IoMdClose } from "react-icons/io";
 // import PhantomWalletButton from "./PhantomWalletButton";
 import "./NavBar.css";
 import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
+import { useWallet } from "@solana/wallet-adapter-react";
+import copyIcon from  "../assets/copy-icon.png"
+import { set } from "@project-serum/anchor/dist/cjs/utils/features";
 
 const NavBar = () => {
+  const {publicKey} = useWallet()
   const [isDropdownVisible, setIsDropdownVisible] = useState(false);
   const [showModal, setShowModal] = useState(false);
+  const [copied, setCopied] = useState(false)
 
   const toggleDropdown = () => {
     setIsDropdownVisible(!isDropdownVisible);
@@ -20,6 +25,15 @@ const NavBar = () => {
     setIsDropdownVisible(false);
   };
 
+  useEffect(() => {
+    if(copied){
+      setTimeout(() => {
+        setCopied(false)
+      }, 1200);
+    }
+  }, [copied])
+  
+
   return (
     <>
       <header className="header">
@@ -27,6 +41,10 @@ const NavBar = () => {
           <Link to="/" className="logo-link">
             <img src="/assets/logo.png" alt="QGEM Logo" className="qgem-logo" />
           </Link>
+          {publicKey ? <div className="copy-box">
+            <p style={{color:"white",marginLeft:"10px"}} >{publicKey?.toString().slice(0,4)+"...."+publicKey.toString().slice(-4)} </p>
+            {!copied ? <img src={copyIcon} alt="copy" onClick={() => {navigator.clipboard.writeText(publicKey?.toString()); setCopied(true) }} /> : <p style={{color:"white"}}>copied!</p>} 
+          </div>: null}
         </div>
         <div className="navbar-container">
           <nav className="navigation">
