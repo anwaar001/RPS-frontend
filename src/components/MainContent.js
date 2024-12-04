78d57925e76e501493504d79cf6acbed0917c7af
anwaar001 <anwaar6260@gmail.com>
2024-12-04 22:58:03 +0500

    just some formatting

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -3,14 +3,14 @@ import { Link } from 'react-router-dom';
 import './MainContent.css';
 import { program } from '../program/game-func';
 import { useAnchorWallet } from '@solana/wallet-adapter-react';
-import copyIcon from  "../assets/copy-icon.png"
+import copyIcon from "../assets/copy-icon.png"
 
 const QGEM_ADDRESS = "6oBcsN8wRze7BjHYXF911YNGTSdG2awYqjxh8YVjZKo2";
 
 const MainContent = ({ games }) => {
   const [showPopup, setShowPopup] = useState(false);
   const [selectedGame, setSelectedGame] = useState(null);
-  const [activeFAQ, setActiveFAQ] = useState(null); 
+  const [activeFAQ, setActiveFAQ] = useState(null);
   const [activeGames, setActiveGames] = useState(0)
   const [inActiveGame, setInActiveGame] = useState(0)
   const [copied, setCopied] = useState(false)
@@ -25,8 +25,8 @@ const MainContent = ({ games }) => {
     // Add more reviews as needed
   ];
 
-   // Placeholder FAQ data
-   const faqs = [
+  // Placeholder FAQ data
+  const faqs = [
     { id: 1, question: "How do I start playing Quantum RPS?", answer: "To start playing, simply connect your wallet and choose 'Quantum RPS' from the game menu." },
     { id: 2, question: "What is GameFi in Quantum Gem?", answer: "GameFi refers to the blend of gaming and finance, leveraging blockchain technology to create value-driven gaming experiences." },
     // Add more FAQs as needed
@@ -54,7 +54,7 @@ const MainContent = ({ games }) => {
     const gallery = document.querySelector('.games-gallery');
     const scrollAmount = 300; // Adjust as needed
     const currentScroll = gallery.scrollLeft;
-  
+
     gallery.scrollTo({
       top: 0,
       left: direction === 'left' ? currentScroll - scrollAmount : currentScroll + scrollAmount,
@@ -62,28 +62,28 @@ const MainContent = ({ games }) => {
     });
   };
 
-  useEffect(()=>{
-    (async()=>{
-      if(wallet){
+  useEffect(() => {
+    (async () => {
+      if (wallet) {
         const pg = program(wallet)
         const games = await pg.account.game.all();
 
-        const active = games.filter((item)=> item.account.isActive==true)
-        const inActive = games.filter((item)=> item.account.isActive==false)
+        const active = games.filter((item) => item.account.isActive == true)
+        const inActive = games.filter((item) => item.account.isActive == false)
 
         setActiveGames(active?.length)
         setInActiveGame(inActive?.length)
 
-        console.log(games,active, inActive)
+        console.log(games, active, inActive)
 
 
       }
     })();
     console.log("run")
-  },[wallet])
-  
+  }, [wallet])
+
   useEffect(() => {
-    if(copied){
+    if (copied) {
       setTimeout(() => {
         setCopied(false)
       }, 1200);
@@ -97,9 +97,9 @@ const MainContent = ({ games }) => {
         <h1 className="intro-heading">Welcome to Quantum Gem</h1>
         <p className="intro-text">Explore the world of peer-to-peer blockchain gaming with Quantum Gem</p>
         <div className="copy-box">
-            <p style={{color:"white"}} >{QGEM_ADDRESS.slice(0,4)+"...."+QGEM_ADDRESS.slice(-4)} </p>
-            {!copied ? <img src={copyIcon} alt="copy" onClick={() => {navigator.clipboard.writeText(QGEM_ADDRESS); setCopied(true) }} /> : <p style={{color:"white"}}>copied!</p>} 
-          </div>
+          <p style={{ color: "white" }} >{QGEM_ADDRESS.slice(0, 4) + "...." + QGEM_ADDRESS.slice(-4)} </p>
+          {!copied ? <img src={copyIcon} alt="copy" onClick={() => { navigator.clipboard.writeText(QGEM_ADDRESS); setCopied(true) }} /> : <p style={{ color: "white" }}>copied!</p>}
+        </div>
         <div onClick={scrollToGamePreviews} className="intro-cta">
           Discover Games
         </div>
@@ -113,26 +113,26 @@ const MainContent = ({ games }) => {
 
       {/* Interactive Game Showcase */}
       <section className="game-showcase-section" id="gamePreviews">
-    <h2 style={{ color: "#9E2AB3" }}>Game Previews</h2> {/* Title "Game Previews" now in purple */}
-    <div className="games-gallery-wrapper">
-        <button className="arrow left-arrow" onClick={() => scrollGallery('left')}>←</button>
-        <div className="games-gallery">
+        <h2 style={{ color: "#9E2AB3" }}>Game Previews</h2> {/* Title "Game Previews" now in purple */}
+        <div className="games-gallery-wrapper">
+          <button className="arrow left-arrow" onClick={() => scrollGallery('left')}>←</button>
+          <div className="games-gallery">
             {games.map(game => (
-                <div key={game.id} className="game-card">
-                    <Link to={`/${game.name.toLowerCase().replace(/\s+/g, '-')}`}>
-                        <img src={`/assets/${game.imageUrl}`} alt={game.name} className="game-image" />
-                    </Link>
-                    <div className="game-details">
-                        <h3 style={{ color: "black" }}>{game.name}</h3> {/* Game name in black */}
-                        <p style={{ color: "black" }}>{game.description}</p> {/* Game description in black */}
-                        <button onClick={() => handleLearnMore(game)} className="learn-more-btn">Learn More</button>
-                    </div>
+              <div key={game.id} className="game-card">
+                <Link to={`/${game.name.toLowerCase().replace(/\s+/g, '-')}`}>
+                  <img src={`/assets/${game.imageUrl}`} alt={game.name} className="game-image" />
+                </Link>
+                <div className="game-details">
+                  <h3 style={{ color: "black" }}>{game.name}</h3> {/* Game name in black */}
+                  <p style={{ color: "black" }}>{game.description}</p> {/* Game description in black */}
+                  <button onClick={() => handleLearnMore(game)} className="learn-more-btn">Learn More</button>
                 </div>
+              </div>
             ))}
+          </div>
+          <button className="arrow right-arrow" onClick={() => scrollGallery('right')}>→</button>
         </div>
-        <button className="arrow right-arrow" onClick={() => scrollGallery('right')}>→</button>
-    </div>
-</section>
+      </section>
 
 
       {/* Game Description Pop-up */}
